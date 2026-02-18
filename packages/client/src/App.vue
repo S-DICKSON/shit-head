2ef45030a9cd6a92d1abc2d368f53a9bbea6ed25
StephenDickson <stephen@symphonycommerce.io>
2026-02-18 20:19:47 +0000

    feat(19-05): safe area CSS and viewport-fit=cover
    - Add viewport-fit=cover to index.html meta viewport tag
    - Define safe area CSS custom properties in style.css using env()
    - Apply safe area padding at App.vue root level using 100dvh min-height

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -4,7 +4,10 @@ import ConnectionStatus from './components/ConnectionStatus.vue';
 </script>
 
 <template>
-  <div class="min-h-screen bg-green-900">
+  <div
+    class="bg-green-900"
+    style="min-height: 100dvh; padding-top: var(--safe-top); padding-bottom: var(--safe-bottom); padding-left: var(--safe-left); padding-right: var(--safe-right);"
+  >
     <RouterView />
     <NotificationToast />
     <ConnectionStatus />
